90d0984490c0eefc10817c6461d56e063255d626
lema <37505127+docyc@users.noreply.github.com>
2025-11-03 12:46:21 +0800

    Update main.dart

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -173,7 +173,7 @@ void runMainApp(bool startService) async {
     windowManager.setOpacity(1);
     windowManager.setTitle(getWindowName());
     // Do not use `windowManager.setResizable()` here.
-    setResizable(!bind.isIncomingOnly());
+    setResizable(false);
   });
 }
 
